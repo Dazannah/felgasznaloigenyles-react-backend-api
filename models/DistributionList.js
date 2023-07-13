9e3975c15f79962db0c543012c0797eef2351c8c
Dazannah <palmal648@gmail.com>
2023-07-13 19:20:30 +0200

    DistributionList converted to class

--- models/DistributionList.js
+++ models/DistributionList.js
@@ -1,58 +1,60 @@
 const requestsDB = require("../db").db("jogosultsagigenylo").collection("requests")
 const distributionDB = require("../db").db("jogosultsagigenylo").collection("distributionLists")
 
-let DistributionList = function (data) {
-  this.data = data
-  this.data.creationData = {
-    userName: data.decodedToken.data.username,
-    createTime: require("../utils.js").getCurrentTime()
-  }
-  this.errors = []
-}
-
-DistributionList.prototype.validateData = async function () {
-  if (this.data.dataToSend.distributionListAddy === "") {
-    this.errors.push(`Terjesztési lista cím megadása kötelező.`)
-  } else {
-    const splitIt = this.data.dataToSend.distributionListAddy.split("@")
-    this.create = {
-      mainAddress: splitIt[0],
-      adresses: []
+class DistributionList {
+  constructor(data) {
+    this.data = data
+    this.data.creationData = {
+      userName: data.decodedToken.data.username,
+      createTime: require("../utils.js").getCurrentTime()
     }
+    this.errors = []
   }
 
-  const isItTaken = await distributionDB.findOne({
-    mainAddress: this.create.mainAddress
-  })
-
-  if (isItTaken) this.errors.push(`Ez a terjesztési lista cím már foglalt.`)
-
-  const keys = Object.keys(this.data.dataToSend).slice(1)
-
-  for (let i = 0; i < keys.length; i++) {
-    if (this.data.dataToSend[keys[i]] === "") {
-      this.errors.push(`${i + 1}. email címet meg kell adni.`)
+  async validateData() {
+    if (this.data.dataToSend.distributionListAddy === "") {
+      this.errors.push(`Terjesztési lista cím megadása kötelező.`)
     } else {
-      const splitIt = this.data.dataToSend[keys[i]].split("@")
-      this.create.adresses.push(splitIt[0])
+      const splitIt = this.data.dataToSend.distributionListAddy.split("@")
+      this.create = {
+        mainAddress: splitIt[0],
+        adresses: []
+      }
     }
-  }
 
-  return this.errors
-}
-
-DistributionList.prototype.createNewDistributionRequest = async function () {
-  try {
-    const insertResult = await requestsDB.insertOne({
-      mainAddress: this.create.mainAddress,
-      adresses: this.create.adresses,
-      ticketCreation: this.data.creationData,
-      process: "Új terjesztési lista"
+    const isItTaken = await distributionDB.findOne({
+      mainAddress: this.create.mainAddress
     })
 
-    return insertResult
-  } catch (err) {
-    return JSON.stringify(err)
+    if (isItTaken) this.errors.push(`Ez a terjesztési lista cím már foglalt.`)
+
+    const keys = Object.keys(this.data.dataToSend).slice(1)
+
+    for (let i = 0; i < keys.length; i++) {
+      if (this.data.dataToSend[keys[i]] === "") {
+        this.errors.push(`${i + 1}. email címet meg kell adni.`)
+      } else {
+        const splitIt = this.data.dataToSend[keys[i]].split("@")
+        this.create.adresses.push(splitIt[0])
+      }
+    }
+
+    return this.errors
+  }
+
+  async createNewDistributionRequest() {
+    try {
+      const insertResult = await requestsDB.insertOne({
+        mainAddress: this.create.mainAddress,
+        adresses: this.create.adresses,
+        ticketCreation: this.data.creationData,
+        process: "Új terjesztési lista"
+      })
+
+      return insertResult
+    } catch (err) {
+      return JSON.stringify(err)
+    }
   }
 }
 
