b09cc83e90e15f0cb0bac4d2bd4866a008ca1a59
Dazannah <palmal648@gmail.com>
2023-07-19 21:07:25 +0200

    dbMigration scripts

---
+++ dbMigrationScripts/requestMigration.js
@@ -0,0 +1,126 @@
+const { MongoClient } = require("mongodb")
+
+const client = new MongoClient("mongodb://root:root@localhost:27017/")
+const arrays = require("./arrays")
+
+const dbFrom = client.db("korhaz-live-copy")
+const dbTo = client.db("jogosultsagigenylo")
+
+const requestsFrom = dbFrom.collection("requests")
+const requestsTo = dbTo.collection("requests")
+
+async function getRequests() {
+  try {
+    const result = await requestsFrom.find().toArray()
+    return result
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function serializeRequests(requests) {
+  //töröltnél mindent kipipált, és nem emelte át a userNames-t
+  const serialiezdRequetsValue = requests.map(request => {
+    const requestKeys = Object.keys(request)
+
+    let tmp = {
+      _id: request._id,
+      personalInformations: {
+        name: request.name,
+        classId: request.dbId,
+        className: request.class,
+        ticketId: request.id,
+        classLeader: request.classLeader,
+        workPost: request.post,
+        workLocation: request.location,
+        validFrom: request.validFrom,
+        validTo: request.validTo
+      },
+      userPermissionsLeft: [],
+      userPermissionsMiddle: [],
+      userPermissionsRight: [],
+      createTextArea: request.request,
+
+      technical: {
+        isTechnical: false,
+        technicalTextArea: ""
+      },
+      ticketCreation: {
+        userName: request.requestedBy,
+        createTime: request.createTime
+      },
+      process: request.process,
+      permission: {
+        allowed: request.permission.permission,
+        permissionNote: request.permission.permissionNote,
+        permissionTime: request.permission.permissionTime,
+        authorizedBy: request.permission.authorizedBy
+      },
+      completed: {
+        userName: request.doneBy,
+        time: request.completionTime
+      },
+      isCompleted: request.completion == "Kész" ? true : false,
+      userId: request.userID,
+      userNames: request.userNames
+    }
+
+    arrays.leftColumn.forEach(element => {
+      let tmpInsert
+
+      if (requestKeys.includes(element.name)) {
+        if (request[element.name] != null || undefined) {
+          tmpInsert = { name: `${element.name}`, value: true }
+        } else {
+          tmpInsert = { name: `${element.name}`, value: false }
+        }
+      } else {
+        tmpInsert = { name: `${element.name}`, value: false }
+      }
+      tmp.userPermissionsLeft.push(tmpInsert)
+    })
+
+    arrays.middleColumn.forEach(element => {
+      let tmpInsert
+
+      if (requestKeys.includes(element.name)) {
+        if (request[element.name] != null || undefined) {
+          tmpInsert = { name: `${element.name}`, value: true }
+        } else {
+          tmpInsert = { name: `${element.name}`, value: false }
+        }
+      } else {
+        tmpInsert = { name: `${element.name}`, value: false }
+      }
+      tmp.userPermissionsMiddle.push(tmpInsert)
+    })
+
+    arrays.rightColumn.forEach(element => {
+      let tmpInsert
+
+      if (requestKeys.includes(element.name)) {
+        if (request[element.name] != null || undefined) {
+          tmpInsert = { name: `${element.name}`, value: true }
+        } else {
+          tmpInsert = { name: `${element.name}`, value: false }
+        }
+      } else {
+        tmpInsert = { name: `${element.name}`, value: false }
+      }
+      tmp.userPermissionsRight.push(tmpInsert)
+    })
+
+    return tmp
+  })
+  return serialiezdRequetsValue
+}
+
+async function main() {
+  const legacyRequests = await getRequests()
+  const serialiezdRequets = await serializeRequests(legacyRequests)
+  const result = await requestsTo.insertMany(serialiezdRequets)
+
+  console.log(result)
+}
+
+main()
---
+++ dbMigrationScripts/userMigration.js
@@ -0,0 +1,99 @@
+const { MongoClient } = require("mongodb")
+
+const client = new MongoClient("mongodb://root:root@localhost:27017/")
+const arrays = require("./arrays")
+
+const dbFrom = client.db("korhaz-live-copy")
+const dbTo = client.db("jogosultsagigenylo")
+
+const usersFrom = dbFrom.collection("users")
+const usersTo = dbTo.collection("users")
+
+function serializeUsers(legacyUsers) {
+  const serializedUsers = legacyUsers.map(user => {
+    const requestKeys = Object.keys(user.userPermissions)
+    const usernamesKey = Object.keys(user.usernames)
+
+    let tmp = {
+      _id: user._id,
+      userNames: user.usernames,
+      personalInformations: {
+        name: user.name,
+        classId: user.dbId,
+        className: user.class,
+        classLeader: user.classLeader,
+        workPost: user.post,
+        workLocation: user.location,
+        validFrom: user.validFrom,
+        validTo: user.validTo
+      },
+      userPermissionsLeft: [],
+      userPermissionsMiddle: [],
+      userPermissionsRight: [],
+      technical: {
+        isTechnical: false,
+        technicalTextArea: ""
+      },
+      status: user.status
+    }
+
+    arrays.leftColumn.forEach(element => {
+      let tmpInsert
+
+      if (usernamesKey.includes(element.name)) {
+        if (user.usernames[element.name] != null || undefined) {
+          tmpInsert = { name: `${element.name}`, value: true }
+        } else {
+          tmpInsert = { name: `${element.name}`, value: false }
+        }
+      } else {
+        tmpInsert = { name: `${element.name}`, value: false }
+      }
+      tmp.userPermissionsLeft.push(tmpInsert)
+    })
+
+    arrays.middleColumn.forEach(element => {
+      let tmpInsert
+
+      if (requestKeys.includes(element.name)) {
+        if (user.userPermissions[element.name] != null || undefined) {
+          tmpInsert = { name: `${element.name}`, value: true }
+        } else {
+          tmpInsert = { name: `${element.name}`, value: false }
+        }
+      } else {
+        tmpInsert = { name: `${element.name}`, value: false }
+      }
+      tmp.userPermissionsMiddle.push(tmpInsert)
+    })
+
+    arrays.rightColumn.forEach(element => {
+      let tmpInsert
+
+      if (requestKeys.includes(element.name)) {
+        if (user.userPermissions[element.name] != null || undefined) {
+          tmpInsert = { name: `${element.name}`, value: true }
+        } else {
+          tmpInsert = { name: `${element.name}`, value: false }
+        }
+      } else {
+        tmpInsert = { name: `${element.name}`, value: false }
+      }
+      tmp.userPermissionsRight.push(tmpInsert)
+    })
+
+    return tmp
+  })
+
+  return serializedUsers
+}
+
+async function main() {
+  const legacyUsers = await usersFrom.find().toArray()
+  const serializedUsers = serializeUsers(legacyUsers)
+  const result = await usersTo.insertMany(serializedUsers)
+
+  console.log(result)
+}
+
+main()
