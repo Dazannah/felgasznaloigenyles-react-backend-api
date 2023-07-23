1ded0a2894dba597e7f88d22fc1b39ea6d7658d3
Dazannah <palmal648@gmail.com>
2023-07-23 16:21:01 +0200

    deleted a const that dont needed

--- controller/searchController.js
+++ controller/searchController.js
@@ -3,8 +3,8 @@ const { Serach } = require("../models/Database")
 async function tableHeadSearch(req, res) {
   try {
     const search = new Serach({ collection: req.body.collection, accessor: req.body.accessor, value: req.body.value, status: req.body.status, userId: req.body.userId, order: req.body.order })
-    const condition = search.getStatusCondition()
-    const result = await search.search(condition)
+    search.getStatusCondition()
+    const result = await search.search()
 
     res.json(result)
   } catch (err) {
