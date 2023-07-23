de951dec163a2476c0b1d6bffe6f7cc070df0918
Dazannah <palmal648@gmail.com>
2023-07-23 13:57:11 +0200

    Login model transformed into ES6 class

--- models/Login.js
+++ models/Login.js
@@ -3,100 +3,102 @@ const dotenv = require("dotenv")
 
 dotenv.config()
 
-let Login = function (data) {
-  this.data = data
-  this.errors = []
-  this.userGroups = [""]
-}
-
-Login.prototype.cleaneUp = function () {
-  if (typeof this.data.username != "string") {
-    this.data.username = ""
+class Login {
+  constructor(data) {
+    this.data = data
+    this.errors = []
+    this.userGroups = [""]
   }
-  this.data.username = this.data.username.trim().toLowerCase()
-}
 
-Login.prototype.validate = function () {
-  if (this.data.username == "" || this.data.username == null) {
-    this.errors.push("Felhasználónév megadása kötelező.")
-  }
-  if (this.data.password == "" || this.data.password == null) {
-    this.errors.push("Jelszó megadása kötelező.")
-  }
-}
-
-Login.prototype.login = function () {
-  return new Promise(async (resolve, reject) => {
-    this.cleaneUp()
-    this.validate()
-
-    if (this.errors.length > 0) {
-      reject(this.errors)
-    } else {
-      try {
-        const result = await this.authenticate()
-        resolve(result)
-      } catch (err) {
-        reject(this.errors)
-      }
+  cleaneUp() {
+    if (typeof this.data.username != "string") {
+      this.data.username = ""
     }
-  })
-}
-
-Login.prototype.authenticate = function () {
-  let usernameWithDomain = this.data.username + process.env.DOMAIN
-  let username = this.data.username
-  let password = this.data.password
-  let errors = this.errors
-  let userGroups = this.userGroups
-
-  let ldapConfig = {
-    url: process.env.LDAPURL,
-    baseDN: process.env.BASEDN,
-    username: usernameWithDomain,
-    password
+    this.data.username = this.data.username.trim().toLowerCase()
   }
 
-  const ad = new ActiveDirectory(ldapConfig)
+  validate() {
+    if (this.data.username == "" || this.data.username == null) {
+      this.errors.push("Felhasználónév megadása kötelező.")
+    }
+    if (this.data.password == "" || this.data.password == null) {
+      this.errors.push("Jelszó megadása kötelező.")
+    }
+  }
 
-  /*let opts = {
-    bindDN: usernameWithDomain,
-    bindCredentials: password
-  }*/
+  login() {
+    return new Promise(async (resolve, reject) => {
+      this.cleaneUp()
+      this.validate()
 
-  return new Promise(async (resolve, reject) => {
-    let runCount = 0
-
-    ad.getGroupMembershipForUser(usernameWithDomain, (err, groups) => {
-      if (err) {
-        const errorMessage = JSON.stringify(err)
-        const errorMessageObject = JSON.parse(errorMessage)
-
-        if (errorMessageObject.lde_dn === null && runCount > 0) errors.push("Hibás flehasználónév/jelszó.")
-        if (errorMessageObject.code === "ENOTFOUND") errors.push("A hitelesítő szerver nem elérhető.")
-        runCount++
-
-        reject(new Error(errors))
-      } else if (!groups) {
-        errors.push("Nincs jogosultságod az alkalmazás használatához.")
-        reject(new Error(errors))
+      if (this.errors.length > 0) {
+        reject(this.errors)
       } else {
-        groups.forEach(element => {
-          if (element.cn == "Tartományfelhasználók") {
-            userGroups[0] = element.cn
-          } //kérelmezők AD csoport neve
-          if (element.cn == "JogosultsagigenyEngedelyezok") {
-            userGroups[1] = element.cn
-          } //engedélyezők AD csoport neve
-          if (element.cn == "JogosultsagigenyAdminisztrator") {
-            userGroups[2] = element.cn
-          } //létrehozók AD csoport neve
-        })
-
-        resolve({ username: username, userGroups: userGroups })
+        try {
+          const result = await this.authenticate()
+          resolve(result)
+        } catch (err) {
+          reject(this.errors)
+        }
       }
     })
-  })
+  }
+
+  authenticate() {
+    let usernameWithDomain = this.data.username + process.env.DOMAIN
+    let username = this.data.username
+    let password = this.data.password
+    let errors = this.errors
+    let userGroups = this.userGroups
+
+    let ldapConfig = {
+      url: process.env.LDAPURL,
+      baseDN: process.env.BASEDN,
+      username: usernameWithDomain,
+      password
+    }
+
+    const ad = new ActiveDirectory(ldapConfig)
+
+    /*let opts = {
+      bindDN: usernameWithDomain,
+      bindCredentials: password
+    }*/
+
+    return new Promise(async (resolve, reject) => {
+      let runCount = 0
+
+      ad.getGroupMembershipForUser(usernameWithDomain, (err, groups) => {
+        if (err) {
+          const errorMessage = JSON.stringify(err)
+          const errorMessageObject = JSON.parse(errorMessage)
+
+          if (errorMessageObject.lde_dn === null && runCount > 0) errors.push("Hibás flehasználónév/jelszó.")
+          if (errorMessageObject.code === "ENOTFOUND") errors.push("A hitelesítő szerver nem elérhető.")
+          runCount++
+
+          reject(new Error(errors))
+        } else if (!groups) {
+          errors.push("Nincs jogosultságod az alkalmazás használatához.")
+          reject(new Error(errors))
+        } else {
+          groups.forEach(element => {
+            if (element.cn == "Tartományfelhasználók") {
+              userGroups[0] = element.cn
+            } //kérelmezők AD csoport neve
+            if (element.cn == "JogosultsagigenyEngedelyezok") {
+              userGroups[1] = element.cn
+            } //engedélyezők AD csoport neve
+            if (element.cn == "JogosultsagigenyAdminisztrator") {
+              userGroups[2] = element.cn
+            } //létrehozók AD csoport neve
+          })
+
+          resolve({ username: username, userGroups: userGroups })
+        }
+      })
+    })
+  }
 }
 
 class Autherization {
@@ -105,7 +107,7 @@ class Autherization {
   }
 
   getAccess(authorizationLevel) {
-    let isAuthorized = false //in prod set it false
+    let isAuthorized = true //in prod set it false
 
     this.userGroups.forEach(group => {
       authorizationLevel.forEach(auth => {
