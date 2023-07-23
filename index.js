c06e9efda9a4abb84d1d90c2fcb15c2567db244c
Dazannah <palmal648@gmail.com>
2023-07-23 14:44:08 +0200

    added catch all error handler middleware

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express')
+const express = require("express")
 const path = require("path")
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
-const server = require('http').createServer(app)
-const router = require('./router')
+const server = require("http").createServer(app)
+const router = require("./router")
 
-app.use('/api', router)
+app.use("/api", router)
 
 app.use(express.static(path.join(__dirname, "dist")))
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
+
+app.use((err, req, res, next) => {
+  res.json(`${err}`)
 })
 
 module.exports = server
