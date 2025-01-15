feb934edb29ce4a2d19c2db330ee1e56e677c61d
dazannah <palmal648@gmail.com>
2025-01-15 14:23:42 +0100

    added sendDailyReport func

--- db.js
+++ db.js
@@ -2,9 +2,17 @@ const dotenv = require("dotenv")
 dotenv.config() //dotenv enviroment variable, a .env fileba mentjük le a mongodb connection stringet, ami a CONNECTIONSTRING ben tároltunk le
 const { MongoClient } = require("mongodb")
 const JSONHandler = require("./models/JSONHandler")
+const Mailer = require("./models/Mailer")
+
+const mailer = new Mailer(process.env.EMAILUSER,
+  process.env.EMAILPASSWORD,
+  process.env.SMTP,
+  process.env.SMTPPORT,
+  true,
+  process.env.EMAILTO)
 
 const client = new MongoClient(process.env.CONNECTIONSTRING)
-const jsonHandler = new JSONHandler()
+const jsonHandler = new JSONHandler(mailer)
 
 let db
 
@@ -14,6 +22,7 @@ async function start() {
   module.exports = client
   const app = require("./index")
   watchMailsJson()
+  //sendDailyReport()
   app.listen(process.env.PORT) //enviroment variable PORT-ban a 3000
 }
 start()
@@ -22,7 +31,14 @@ function watchMailsJson(){
   setTimeout(async ()=>{
     jsonHandler.sendEmailIfAny()
     watchMailsJson()
-}, 1000 * 60 * 5)
+  }, 1000 * 60 * 5)
+}
+
+function sendDailyReport(){
+  setTimeout(async ()=>{
+    const {subject, plainText, htmlText} = mailer.parseDailyReportEmail()
+    mailer.sendMail(subject, plainText, htmlText, process.env.DAILYREPORTTO)
+  }, 1000 * 60 * 60 * 24)
 }
 
 module.exports = db
